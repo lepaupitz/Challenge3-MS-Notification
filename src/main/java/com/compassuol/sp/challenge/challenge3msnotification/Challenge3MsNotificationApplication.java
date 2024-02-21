ba936924a0eccf3320b0c61dc3edc87499b48b3a
Leandro Paupitz <lepaupitz2@gmail.com>
2024-02-21 14:47:28 -0300

    first commit

---
+++ src/main/java/com/compassuol/sp/challenge/challenge3msnotification/Challenge3MsNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.challenge3msnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge3MsNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge3MsNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compassuol/sp/challenge/challenge3msnotification/Challenge3MsNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.challenge3msnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge3MsNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
